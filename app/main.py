10a3738338da11a59e7a8fcd98cfd5fd25d92bfd
Simon Peter <simonpi442@gmail.com>
2026-09-21 17:08:20 +0100

    Update FastAPI application

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ app = FastAPI(title="Jenkins FastAPI Demo")
 @app.get("/")
 def home():
     return {
-        "message": "Hello from FastAPI!", 
+        "message": "Hello from Jenkins CI/CD!", 
         "version": "1.0.0"
     }
 
